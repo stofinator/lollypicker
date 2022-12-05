4deb2ffa74acd703db6b4a4298f261f3b615d92f
stofinator <117523237+stofinator@users.noreply.github.com>
2022-12-05 15:50:25 +1100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 class CustomerType:
-    def __init__(self, bags: list, population: int, name:str):
+    def __init__(self, bags: list, population: int, name: str):
         self.bags = bags
         self.bag_people = {}
         self.name = name
@@ -31,7 +31,7 @@ class CustomerType:
 
 
 class Bag:
-    def __init__(self, lollies: list, name:str):
+    def __init__(self, lollies: list, name: str):
         self.lollies = lollies
         self.name = name
 
@@ -43,8 +43,7 @@ class Bag:
         for lolly in self.lollies:
             if lolly.quantity < 1:
                 return True
-            else:
-                return False
+        return False
 
     def unpick(self):
         for lolly in self.lollies:
@@ -68,40 +67,54 @@ def MBR(CustomerTypes) -> CustomerType:
     return min_customer_type
 
 
-apple = Lolly(10)
-strawberry = Lolly(7)
+apple = Lolly(6)
+strawberry = Lolly(5)
 
-justApple = Bag([apple],"just apple")
+justApple = Bag([apple], "just apple")
 appleStraw = Bag([apple, strawberry], "apple and strawberry")
 
-CustomerType1 = CustomerType([justApple, appleStraw], 5, "Strawberry and apple or plain apple.")
-CustomerType2 = CustomerType([appleStraw], 5, "Strawberry and apple only.")
+CustomerType1 = CustomerType([justApple, appleStraw], 4, "Strawberry and apple or plain apple.")
+CustomerType2 = CustomerType([appleStraw], 2, "Strawberry and apple only.")
 
-completely_all_customers = [CustomerType1,CustomerType2]
+completely_all_customers = [CustomerType1, CustomerType2]
 allCustomers = [CustomerType1, CustomerType2]
 
 
-def bag_assigner(all_customers):
-    min_customer_type = MBR(all_customers)
+def bag_assigner():
+    min_customer_type = MBR(allCustomers)
     if not min_customer_type:
         print("Solution:")
         for CustomerType in completely_all_customers:
             CustomerType.print_assignments()
         print("End solution")
         return
+    else:
+        allCustomers.remove(min_customer_type)
+        customer_type_assigner(min_customer_type, min_customer_type.bags)
+        allCustomers.append(min_customer_type)
 
-    if min_customer_type.population == 0:
-        all_customers.remove(min_customer_type)
-        bag_assigner(all_customers)
-        all_customers.append(min_customer_type)
+
+def customer_type_assigner(customer_type: CustomerType, bags: list):
+    if len(bags) == 0:
         return
 
-    for bag in min_customer_type.bags:
-        if not bag.check_if_empty():
-            min_customer_type.pick_bag(bag)
-            bag_assigner(all_customers)
-            min_customer_type.unpick_bag(bag)
-    return
+    next_bag_level = bags.copy()
+    j = 0
+    bag = bags[0]
+    next_bag_level.remove(bag)
+
+    customer_type_assigner(customer_type, next_bag_level)  # 0th call (not chosen at all)
+    while not bag.check_if_empty():
+        j += 1
+        customer_type.pick_bag(bag)
+        if customer_type.population == 0:
+            bag_assigner()
+        customer_type_assigner(customer_type, next_bag_level)
+
+    while j != 0:
+        customer_type.unpick_bag(bag)
+        j -= 1
 
 
-bag_assigner(allCustomers)
+
+bag_assigner()
